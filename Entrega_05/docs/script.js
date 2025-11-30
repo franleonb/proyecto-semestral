24da522ed36feed26b1b90782a18b5f59d1d336e
franleonb <franleonb@uc.cl>
2025-11-30 16:37:13 -0300

    datos

--- Entrega_05/docs/script.js
+++ Entrega_05/docs/script.js
@@ -1,1 +1,13 @@
+const canastas = document.querySelectorAll('.canasta');
+
+canastas.forEach(canasta => {
+  canasta.addEventListener('click', () => {
+    const info = canasta.querySelector('.info');
+    if (info.style.display === 'block') {
+      info.style.display = 'none';
+    } else {
+      info.style.display = 'block';
+    }
+  });
+});
 
